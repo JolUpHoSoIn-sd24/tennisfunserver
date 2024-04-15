16fae8304b55fe37ea7988d3fe0e2cfb8738f29b
Gwan Ju <113610239+kkj6235@users.noreply.github.com>
2024-04-16 00:24:02 +0900

    feat: "User" database schema

---
+++ src/main/java/JolUpHoSoIn/TennisFun_Server/user/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package JolUpHoSoIn.TennisFun_Server.user.repository;
+
+import JolUpHoSoIn.TennisFun_Server.user.data.entity.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User,String> {
+}
