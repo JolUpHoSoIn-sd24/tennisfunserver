d5daf9a535350abedf250edc52309f964c849c92
SeungHun Han <shhan730@gmail.com>
2024-05-05 21:45:19 +0900

    chore: Mail and Auth settings

--- src/main/java/joluphosoin/tennisfunserver/TennisFunServerApplication.java
+++ src/main/java/joluphosoin/tennisfunserver/TennisFunServerApplication.java
@@ -4,8 +4,9 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @OpenAPIDefinition(
 		servers = {
 				@Server(url = "/", description = "Default Server url")
--- src/main/java/joluphosoin/tennisfunserver/user/repository/UserRepository.java
+++ src/main/java/joluphosoin/tennisfunserver/user/repository/UserRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface UserRepository extends MongoRepository<User,String> {
     boolean existsByEmailId(String emailId);
     Optional<User> findByEmailVerificationToken(String token);
+    Optional<Object> findByEmailId(String email);
 }
