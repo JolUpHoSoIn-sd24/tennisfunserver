d120888bc7a36bf7497cf59cd35994d0984cbf6f
Gwan Ju <113610239+kkj6235@users.noreply.github.com>
2024-05-09 15:34:40 +0900

    fix : HTTPStatus code 200 -> 201

--- src/main/java/joluphosoin/tennisfunserver/business/controller/CourtController.java
+++ src/main/java/joluphosoin/tennisfunserver/business/controller/CourtController.java
@@ -5,6 +5,7 @@ import joluphosoin.tennisfunserver.business.data.dto.CourtReqDto;
 import joluphosoin.tennisfunserver.business.data.dto.CourtTimeSlotReqDto;
 import joluphosoin.tennisfunserver.business.service.CourtService;
 import joluphosoin.tennisfunserver.payload.ApiResult;
+import joluphosoin.tennisfunserver.payload.code.status.SuccessStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,13 +21,13 @@ public class CourtController {
     @PostMapping("/")
     public ApiResult<String> registerCourt(@RequestBody @Valid CourtReqDto courtReqDto){
         courtService.registerCourt(courtReqDto);
-        return ApiResult.onSuccess("테니스장 정보가 성공적으로 등록되었습니다.");
+        return ApiResult.onSuccess(SuccessStatus.CREATED,"테니스장 정보가 성공적으로 등록되었습니다.");
     }
 
     @PostMapping("/availability")
     public ApiResult<String> registerCourtTimeSlot(@RequestBody @Valid CourtTimeSlotReqDto courtTimeSlotReqDto){
         courtService.registerCourtTimeSlot(courtTimeSlotReqDto);
-        return ApiResult.onSuccess("코트별 대여 가능 시간 및 비용이 성공적으로 등록되었습니다.");
+        return ApiResult.onSuccess(SuccessStatus.CREATED,"코트별 대여 가능 시간 및 비용이 성공적으로 등록되었습니다.");
     }
 
 
