8cf324b6f78aa7271c9e3fb9b0e56e797719ac8b
Gwan Ju <113610239+kkj6235@users.noreply.github.com>
2024-05-22 01:03:29 +0900

    refactor: change starttime type to string in timeslotdto

--- src/main/java/joluphosoin/tennisfunserver/business/data/dto/TimeSlotDto.java
+++ src/main/java/joluphosoin/tennisfunserver/business/data/dto/TimeSlotDto.java
@@ -6,20 +6,24 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
+
 @Getter
 @Builder
 @AllArgsConstructor
 public class TimeSlotDto {
 
-    private Date startTime;
+    private String startTime;
 
     @Setter
     private DayTimeSlot.ReservationStatus status;
 
     public static TimeSlotDto toDto(Date startTime){
+        SimpleDateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
         return TimeSlotDto.builder()
-                .startTime(startTime)
+                .startTime(isoFormat.format(startTime))
                 .status(DayTimeSlot.ReservationStatus.NOT_OPEN)
                 .build();
     }
