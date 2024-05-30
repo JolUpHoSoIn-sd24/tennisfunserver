e41f032f40d4ff188fd2efe8078b1a0050b353a0
Gwan Ju <113610239+kkj6235@users.noreply.github.com>
2024-05-30 22:43:14 +0900

    refactor : apply Swagger schema to UserResDto

--- src/main/java/joluphosoin/tennisfunserver/user/data/dto/UserResDto.java
+++ src/main/java/joluphosoin/tennisfunserver/user/data/dto/UserResDto.java
@@ -1,5 +1,6 @@
 package joluphosoin.tennisfunserver.user.data.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import joluphosoin.tennisfunserver.user.data.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,31 +17,43 @@ import java.util.List;
 @AllArgsConstructor
 public class UserResDto {
     @MongoId
+    @Schema(example ="6646cd0991733e1b2d1e8b53")
     private String id;
 
     @Indexed
+    @Schema(example ="이은정")
     private String name;
 
     @Indexed(unique = true)
+    @Schema(example ="4@seunglab.dev")
     private String emailId;
 
+    @Schema(example ="82")
     private Integer age; // 나이 필드 추가
 
+    @Schema(example ="FEMALE")
     private String gender; // 성별 필드 추가
 
     @GeoSpatialIndexed(type = GeoSpatialIndexType.GEO_2DSPHERE)
+    @Schema(example ="{ \"x\": 127.07134, \"y\": 37.251521\n}")
     private Point location;
 
+    @Schema(example ="3.0")
     private Double maxDistance;
 
+    @Schema(example ="[\"664704a7cb787760b7c0220f\"] ")
     private List<String> dislikedCourts;
 
+    @Schema(example ="4.5")
     private double ntrp;
 
+    @Schema(example ="36.5")
     private Double mannerScore;
 
+    @Schema(example ="[ 6646cd0991733e1b2d1e8b53 ]")
     private List<String> clubIds;
 
+    @Schema(example ="true")
     private boolean emailVerified;
 
     public static UserResDto toDto(User user){
