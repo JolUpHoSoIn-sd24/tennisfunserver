35b58af232bc3115873f41e8054e301d6906d18d
Gwan Ju <113610239+kkj6235@users.noreply.github.com>
2024-06-03 01:33:50 +0900

    fix : delete localhost:3000 CorsMapping

--- src/main/java/joluphosoin/tennisfunserver/config/WebMvcConfig.java
+++ src/main/java/joluphosoin/tennisfunserver/config/WebMvcConfig.java
@@ -2,7 +2,6 @@ package joluphosoin.tennisfunserver.config;
 
 import joluphosoin.tennisfunserver.intercepter.LoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -31,14 +30,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
                         "/api/admin/login");
     }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:3000")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+//    }
 
 }
 
